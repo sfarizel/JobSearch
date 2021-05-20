46358d7fae8a2a3eaa2d21161ae7e4a7ad787ec7
= <=>
2021-05-20 20:47:16 -0300

    Inicio do Projeto JobSearch

---
+++ AppJobSearch/JobSearch.API/Controllers/JobsController.cs
@@ -0,0 +1,73 @@
+﻿using JobSearch.API.Database;
+using JobSearch.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobSearch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        private int recordsByPage = 5;
+
+        private JobSearchContext _data;
+
+        public JobsController(JobSearchContext data)
+        {
+            _data = data;
+        }
+
+        // /api/Jobs/id
+        [HttpGet("{id}")]
+        public IActionResult GetJob(int id)
+        {
+            Job jobDb = _data.Jobs.Find(id);
+            if (jobDb == null)
+            {
+                return NotFound();
+            }
+            return new JsonResult(jobDb);
+        }
+
+        [HttpGet]
+        public IEnumerable<Job> GetJobs(string word, string cityState, int numberOfPage = 1)
+        {
+            if (word == null)
+                word = "";
+
+            if (cityState == null)
+                cityState = "";
+
+            return _data.Jobs
+                .Where(a =>
+                a.PublicationDate >= DateTime.Now.AddDays(-15) &&
+                a.CityState.ToLower().Contains(cityState.ToLower()) &&
+                    (
+                        a.JobTitle.ToLower().Contains(word.ToLower()) ||
+                        a.TecnologyTools.ToLower().Contains(word.ToLower()) ||
+                        a.Company.ToLower().Contains(word.ToLower())
+                    )
+            )
+                .Skip(recordsByPage * (numberOfPage - 1))
+                .Take(recordsByPage)
+                .ToList<Job>();
+        }
+
+        public IActionResult AddJob(Job job)
+        {
+            //TODO - Add validação 
+
+            job.PublicationDate = DateTime.Now;
+            _data.Jobs.Add(job);
+            _data.SaveChanges();
+
+            return CreatedAtAction("GetJob", new { id = job.Id }, job);
+        }
+
+    }
+}
---
+++ AppJobSearch/JobSearch.API/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using JobSearch.API.Database;
+using JobSearch.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobSearch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private JobSearchContext _data;
+        public UsersController(JobSearchContext data)
+        {
+            _data = data;
+        }
+
+        /*
+         * http://seusite.com.br/api/users?email=sfarizel@gmail.com&password=123456
+         */
+        [HttpGet]
+        public IActionResult GetUser(string email, string password)
+        {
+            User userDb = _data.Users.FirstOrDefault(a => a.Email == email && a.Password == password);
+            if (userDb == null)
+            {
+                return NotFound(); //HTTP - 404
+            }
+
+            return new JsonResult(userDb);
+            
+        }
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            //TODO - Validação...
+            _data.Users.Add(user);
+            _data.SaveChanges(); //Ver o padrão Unit of Work
+
+            return CreatedAtAction(nameof(GetUser), new { email = user.Email, password = user.Password },user); //201 - Created 
+        }
+    }
+}
